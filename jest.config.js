6656dab8644be6f70ed01b0c59f8f162ddc02978
Kyle Miller <kmiller@systemautomation.com>
2025-01-30 14:16:21 -0500

    clone @elastic/handlebars: initial commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+/*
+ * Elasticsearch B.V licenses this file to you under the MIT License.
+ * See `packages/kbn-handlebars/LICENSE` for more information.
+ */
+
+module.exports = {
+  rootDir: '.',
+  roots: ['<rootDir>/src/spec'],
+};
